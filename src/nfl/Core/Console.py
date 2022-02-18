212df273941ec6be7c8e7c7a263b921e9fc8adc4
Will Maethner <wmaethner@gmail.com>
2022-02-17 20:24:47 -0500

    Add tensor flow predicters

--- src/nfl/Core/Console.py
+++ src/nfl/Core/Console.py
@@ -16,8 +16,10 @@ import matplotlib.pyplot as plt
 from nfl.Data.DataScraper import Data_Scraper
 from nfl.Data.TeamManager import Team_Manager
 
-from nfl.Predicters.Predicters import (Offense_Correlation, Team_Stats_Only_Correlation, 
-                                   Offense_Minus_Defense_Correlation)
+from nfl.Predicters.Predicters import (Offense_Correlation, 
+                                       Team_Stats_Only_Correlation, 
+                                       TensorFlowBasic,
+                                       TensorFlowBothStats)
 from nfl.Predicters.Analyzer import analyze_predicter
 
 
@@ -55,169 +57,107 @@ def explore_dict(dictionary):
 def make_prediction(year, home, away):
     off = Offense_Correlation(year)
     stats = Team_Stats_Only_Correlation(year)
-    off_def = Offense_Minus_Defense_Correlation(year)
+    # off_def = Offense_Minus_Defense_Correlation(year)
     print(f'Matchup: {home} - {away}')
     print(off.predict_winner(home, away))
     print(stats.predict_winner(home, away))
-    print(off_def.predict_winner(home, away))
+    # print(off_def.predict_winner(home, away))
 
-def analyze_predicters(data_year):
-    print(f'Data from year {data_year}')
-    off = Offense_Correlation([data_year])
-    stats = Team_Stats_Only_Correlation([data_year])
-    off_def = Offense_Minus_Defense_Correlation([data_year])
+def analyze_predicters(train_years, test_years):
+    off = Offense_Correlation(train_years)
+    stats = Team_Stats_Only_Correlation(train_years)
+    tfb = TensorFlowBasic(train_years)
+    tfbs = TensorFlowBothStats(train_years)
+
     
-    for year in range(2019, 2022):
-        print(f'{year}')
-        analyze_predicter(off, [year])
-        analyze_predicter(stats, [year])
-        analyze_predicter(off_def, [year])
+    analyze_predicter(off, test_years)
+    analyze_predicter(stats, test_years)
+    analyze_predicter(tfb, test_years)
+    analyze_predicter(tfbs, test_years)
+
+       
+def save_stats_to_csvs(years):
+    tm = Team_Manager()
+
+    for year in years:   
+        data = {}
+        for index, t in enumerate(tm.teams):
+            data[t.id_num] = tm.get_teams_stats(t.id_num, year, 1)
         
+        offense = pd.concat({key: x.iloc[[0]] for key, x in data.items()}, axis=0).reset_index(level=1, drop=True)
+        defense = pd.concat({key: x.iloc[[1]] for key, x in data.items()}, axis=0).reset_index(level=1, drop=True)
+        
+        offense.rename(index={team: tm.get_team(team).name for team in range(32)}, inplace=True)
+        defense.rename(index={team: tm.get_team(team).name for team in range(32)}, inplace=True)
+        
+        offense.to_csv(f'CSVs/{year}_offense.csv')
+        defense.to_csv(f'CSVs/{year}_defense.csv')  
 
 
 def main():
     pd.set_option('display.max_columns', None)
     
-    # tm = Team_Manager()
-    # print(len(tm.teams))
+    ds = Data_Scraper()
+    tm = Team_Manager()
+   
+    # train_years = [2017]
+    # test_years = [2017]
+    # for i in range(5):
+    #     tfb = TensorFlowBasic(train_years)
+    #     tfbs = TensorFlowBothStats(train_years)
+    #     analyze_predicter(tfb, test_years)
+    #     analyze_predicter(tfbs, test_years)
+    #     print('========================')
     # return
+    
+    # analyze_predicters([2017,2018,2019,2020,2021], [2020,2021])
+    
+    off_corr = Team_Stats_Only_Correlation([2021])
+    analyze_predicter(off_corr, [2021])
+    # tfb = TensorFlowBasic(train_years)
+    # analyze_predicter(tfb, test_years)
+    return
+    
+    # print(tfb.train_data)
+    # print(len(tfb.train_results))
+    tfb = TensorFlowBasic([2017])
+    total = 0
+    total_right = 0
+    schedule = ds.get_schedule(2021)
+    for index, row in schedule.iterrows():
+        home = tm.get_team_id(row['Home'])
+        away = tm.get_team_id(row['Away'])
+        results = tfb.predict_winner(home, away, 2021)
+        predicted = np.argmax(results)
+        actual = 1 if tm.get_team_id(row["Winner/tie"]) == home else 0
+        
+        total += 1
+        total_right += 1 if predicted == actual else 0
+        
+        # print(f'{row["Home"]} vs {row["Away"]}: Predicted: {predicted} Actual: {actual}')   
+    print(f'{total_right}/{total}={total_right/total}')
+    
+    return
+    
+    results = tfb.predict_winner(3,4)
+    print(results)
+    print(results[0][0])
+    print('{}: {:2.0f}% vs {}: {:2.0f}%'.format(tm.get_team(3).name,
+                                                100*results[0][0],
+                                                tm.get_team(4).name,
+                                                100*results[0][1]))
+    # print(results)
+    # predictions = model(x_train[:1]).numpy()
+    return
     
     ds = Data_Scraper()
     print(ds.get_schedule(2021))
     return
-    
-    # ds = Data_Scraper()
-    # stats = ds.get_teams_stats('crd',2021,1)
-    # print(stats)
-    # df = pd.DataFrame(stats).T
-    # df.columns = ds.data_columns()
-    # print(df)
-    
-    # data = start_timer('Team Manager Initialize')
-    # tm = Team_Manager()
-    # stop_timer(data, True)
-    # print(tm.get_teams_stats(1,2021,1))
-
-    
-    # url = PFR_BASE_URL + f"/teams/crd/2021.htm"
-    # soup = parse_page(url)
-    # table = get_table_by_id(soup, 'team_stats')
-    # stats = parse_table(table)
-    # print(stats)
-    
-
-    # print(stats)
+   
     
    
-    # for year in range(2021, 2022):
-    #     analyze_predicters(year)
-    # analyze_predicters(2021)
-    
-    # x = [1,2,3,4]
-    # y = [3, 8, 1, 10]
-      
-    # df = pd.DataFrame(y, index=x, columns=['One'])
-    # print(df)
-    # y = [5, 1, 2, 5]
-    # # df = df.append(pd.DataFrame(y, index=x, columns=['Two']))
-    # df.insert(len(df.columns), 'Two', y)
-    # print(df)
-    # df.plot(y=['One','Two'], kind="bar")
-
-    # return
-    # df.plot()
-    # # plot lines
-    # plt.bar(x,y, label = "line 1")
-    # y = np.array([5, 1, 2, 5])
-    # plt.bar(x,y, label = "line 2")
-    
-    # # plt.plot(x, np.sin(x), label = "curve 1")
-    # # plt.plot(x, np.cos(x), label = "curve 2")
-    # plt.legend()
-    # plt.show()
-    # return
-    
-    
-    five_years = list(range(2017,2022))
-    result_df = pd.DataFrame()
-    off_corr_stats = []
-    all_stats_corr_stats = []
-    for rng in range(0, 10):
-        print(rng)
-        years = list(range(2021-rng, 2022))
-        off_corr = Offense_Correlation(years)
-        both_corr = Team_Stats_Only_Correlation(years)
-        # difference_corr = Offense_Minus_Defense_Correlation(years)
-        
-        _,_,percentage = analyze_predicter(off_corr, five_years, False)
-        off_corr_stats.append(percentage)
-        
-        _,_,percentage = analyze_predicter(both_corr, five_years, False)
-        all_stats_corr_stats.append(percentage)
-
-        # _,_,percentage = analyze_predicter(difference_corr, five_years, False)
-        # diff_corr_stats.append(percentage)
-    
-    results = {'Offense Correlation': off_corr_stats, 
-               'All Stats Correlation': all_stats_corr_stats}
-    result_df = pd.DataFrame(results)
-    result_df.plot(y=['Offense Correlation',
-                      'All Stats Correlation'], ylim=[0.5,0.7])
-    return 
-    
     
 
-    off = Offense_Correlation([2019,2020,2021])
-    off1 = Offense_Correlation([2021])
-    print('2020')
-    analyze_predicter(off, 2020)
-    analyze_predicter(off1, 2020)
-    print('2021')
-    analyze_predicter(off, 2021)
-    analyze_predicter(off1, 2021)
-    return
-    
-    # for years in [[2021], [2020,2021], [2019,2020,2021]]:
-    #     off = Offense_Correlation(years)
-    #     for year in [2019,2020,2021]:
-    #         analyze_predicter(off, year)
-            
-    # return
-    
-    
-    # # make_prediction(2021, 'Los Angeles Rams', 'Cincinnati Bengals')
-    # return
-    
-    
-    
-    
-    predicters = []
-    years = [2019, 2020, 2021]
-    for year in years:
-        predicters.append(Offense_Correlation(year))
-        
-    print("Offense Correlation")
-    for predicter in predicters:
-        analyze_predicter(predicter)
-
-    predicters = []
-    for year in years:
-        predicters.append(Team_Stats_Only_Correlation(year))
-        
-    print("Team Stats Correlation")
-    for predicter in predicters:
-        analyze_predicter(predicter)
-
-    predicters = []
-    for year in years:
-        predicters.append(Offense_Minus_Defense_Correlation(year))
-        
-    print("Offense Minus Defense Correlation")
-    for predicter in predicters:
-        analyze_predicter(predicter)
-
-    # print(predictor.predict_winner("Arizona Cardinals", "Atlanta Falcons"))
-    return
 
 
 if __name__ == "__main__":
--- src/nfl/Predicters/Predicters.py
+++ src/nfl/Predicters/Predicters.py
@@ -7,16 +7,19 @@ Created on Sun Feb  6 20:30:48 2022
 """
 
 import pandas as pd
+import tensorflow as tf
+import numpy as np
 
-from nfl.Core.utilities import (printProgressBar)
+from nfl.Core.utilities import (printProgressBar, start_timer, stop_timer)
 from nfl.Data.TeamManager import Team_Manager
+from nfl.Data.DataScraper import Data_Scraper
 
 def get_all_stats(year):
     tm = Team_Manager()
     data = {}
-    printProgressBar(0, len(tm.teams), prefix = 'Progress:', suffix = 'Complete', length = 50)
+    # printProgressBar(0, len(tm.teams), prefix = 'Progress:', suffix = 'Complete', length = 50)
     for index, t in enumerate(tm.teams):
-        printProgressBar(index + 1, len(tm.teams), prefix = 'Progress:', suffix = 'Complete', length = 50)
+        # printProgressBar(index + 1, len(tm.teams), prefix = 'Progress:', suffix = 'Complete', length = 50)
         data[t.id_num] = tm.get_teams_stats(t.id_num, year, 1)
     return data
 
@@ -37,41 +40,58 @@ def get_mean_stats(years, row_index):
 
 def get_correlation_normalized_stats(averaged_stats):
     stats_normal = (averaged_stats-averaged_stats.min())/(averaged_stats.max()-averaged_stats.min())
+    stats_normal = stats_normal.fillna(0)
     corr = averaged_stats.corr() 
     win_corr = corr.iloc[0,3:]
     return (win_corr, stats_normal)
+
+def normalized_stats(year, offense = True):
+    stats = get_mean_stats([year], 0 if offense else 1)
+    normal = (stats-stats.min())/(stats.max()-stats.min())
+    return normal.fillna(0)
 
 def get_winner_percentages(home, home_score, away, away_score):
     total = home_score + away_score
     return home if (home_score > away_score) else away, (home_score/total, away_score/total)
 
-class Offense_Correlation:
+class YearsNormalStats:
+    def __init__(self, year):
+        self.offense = normalized_stats(year)
+        self.defense = normalized_stats(year, False)
+
+class PredicterBase:
     def __init__(self, years):
         self.years = years
-        self.team_offense_scores = {}
-        self.tm = Team_Manager()
+        self.normalized_stats = {}
+        
+        for year in years:
+            self.normalized_stats[year] = YearsNormalStats(year)
+
+class Offense_Correlation(PredicterBase):
+    def __init__(self, years):
+        PredicterBase.__init__(self, years)
         
         df_means = get_mean_stats(years, 0)
-
         win_corr, offense_normal = get_correlation_normalized_stats(df_means)
         
-        for team in offense_normal.index:
-            total = 0
-            for key in win_corr.index:
-                total += win_corr[key] * offense_normal.loc[team, key]
-            self.team_offense_scores[team] = total
+        self.correlations = {}
+        self.correlations['Offense'] = win_corr
+        
     
-    def predict_winner(self, home, away):
-        home_score = self.team_offense_scores[self.tm.get_team_id(home)]
-        away_score = self.team_offense_scores[self.tm.get_team_id(away)]
+    def predict_winner(self, home, away, year):
+        offense_normal = self.normalized_stats[year].offense
+        
+        home_score = 0
+        away_score = 0
+        for key in self.correlations['Offense'].index:
+            home_score += self.correlations['Offense'][key] * offense_normal.loc[home, key]
+            away_score += self.correlations['Offense'][key] * offense_normal.loc[away, key]
             
         return get_winner_percentages(home, home_score, away, away_score)
     
 class Team_Stats_Only_Correlation:
     def __init__(self, years):
-        self.years = years
-        self.team_scores = {}
-        self.tm = Team_Manager()
+        PredicterBase.__init__(self, years)
         
         df_means_off = get_mean_stats(years, 0)
         df_means_def = get_mean_stats(years, 1)
@@ -79,46 +99,157 @@ class Team_Stats_Only_Correlation:
         offense_corr, offense_normal = get_correlation_normalized_stats(df_means_off)
         defense_corr, defense_normal = get_correlation_normalized_stats(df_means_def)
         
-        
-        for team in offense_normal.index:
-            total = 0
-            for key in offense_corr.index:
-                total += offense_corr[key] * offense_normal.loc[team, key]
-                total += defense_corr[key] * defense_normal.loc[team, key]
-            self.team_scores[team] = total
+        self.correlations = {}
+        self.correlations['Offense'] = offense_corr
+        self.correlations['Defense'] = defense_corr
+
     
-    def predict_winner(self, home, away):
-        home_score = self.team_scores[self.tm.get_team_id(home)]
-        away_score = self.team_scores[self.tm.get_team_id(away)]
+    def predict_winner(self, home, away, year):
+        offense_normal = self.normalized_stats[year].offense
+        defense_normal = self.normalized_stats[year].defense
+        
+        home_score = 0
+        away_score = 0
+        for key in self.correlations['Offense'].index:
+            home_score += self.correlations['Offense'][key] * offense_normal.loc[home, key]
+            home_score += self.correlations['Defense'][key] * defense_normal.loc[home, key]
+            away_score += self.correlations['Offense'][key] * offense_normal.loc[away, key]
+            away_score += self.correlations['Defense'][key] * defense_normal.loc[away, key]
             
         return get_winner_percentages(home, home_score, away, away_score)
-    
-# Pretty sure this just ends up being the same as the All Stats one mathematically
-# The results always end up the exact same. Probably should remove.
-class Offense_Minus_Defense_Correlation:
+       
+class TensorFlowBasic:
     def __init__(self, years):
-        self.years = years
-        self.team_offense_scores = {}
-        self.team_defense_scores = {}
+        self.train_data = []
+        self.train_results = []
+        self.test_data = []
+        self.test_results = []
+        
         self.tm = Team_Manager()
+        self.stats = {}
         
-        df_means_off = get_mean_stats(years, 0)
-        df_means_def = get_mean_stats(years, 1)
+        ds = Data_Scraper()       
         
-        offense_corr, offense_normal = get_correlation_normalized_stats(df_means_off)
-        defense_corr, defense_normal = get_correlation_normalized_stats(df_means_def)
-        
-        for team in offense_normal.index:
-            total_off = 0
-            total_def = 0
-            for key in offense_corr.index:
-                total_off +=  offense_corr[key] * offense_normal.loc[team, key]
-                total_def +=  defense_corr[key] * defense_normal.loc[team, key]
-            self.team_offense_scores[team] = total_off
-            self.team_defense_scores[team] = total_def
-    
-    def predict_winner(self, home, away):
-        home_score = self.team_offense_scores[self.tm.get_team_id(home)] - self.team_defense_scores[self.tm.get_team_id(away)]
-        away_score = self.team_offense_scores[self.tm.get_team_id(away)] - self.team_defense_scores[self.tm.get_team_id(home)]
+        for year in years:    
+            offense_normal = normalized_stats(year)
+           
+            offense_numpy = offense_normal.to_numpy()
+            self.stats[year] = offense_numpy
             
-        return get_winner_percentages(home, home_score, away, away_score)
+            schedule = ds.get_schedule(year)
+            for index, row in schedule.iterrows():
+                home = self.tm.get_team_id(row['Home'])
+                away = self.tm.get_team_id(row['Away'])
+                winner = 1 if self.tm.get_team_id(row['Winner/tie']) == home else 0
+                
+                data = [offense_numpy[home].tolist(), offense_numpy[away].tolist()]           
+                
+                self.train_data.append(data)
+                self.train_results.append(winner)
+          
+    
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(2, len(self.train_data[0][0]))),
+            tf.keras.layers.Dense(10, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(2)
+            ])
+        
+        self.model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+
+        self.model.fit(self.train_data, self.train_results, epochs=50, verbose=False)      
+        self.probability_model = tf.keras.Sequential([self.model, tf.keras.layers.Softmax()])
+        
+    def predict_winner(self, home, away, year):
+        if not year in self.stats.keys():
+            print(f'{year} was not included in data')
+            return
+        
+        data = [self.stats[year][home].tolist(), self.stats[year][away].tolist()]     
+        data_exp = (np.expand_dims(data,0))
+        
+        result = self.probability_model.predict(data_exp)
+     
+        return home if np.argmax(result) == 1 else away, np.max(result)
+        
+  
+class TensorFlowBothStats:
+    def __init__(self, years):
+        self.train_data = []
+        self.train_results = []
+        self.test_data = []
+        self.test_results = []
+        
+        self.tm = Team_Manager()
+        self.stats = {}
+        
+        ds = Data_Scraper()       
+        
+        for year in years:    
+            offense_normal = normalized_stats(year)
+            defense_normal = normalized_stats(year, False)
+            
+            offense_numpy = offense_normal.to_numpy()
+            defense_numpy = defense_normal.to_numpy()
+            self.stats[year] = {}
+            self.stats[year]['Offense'] = offense_numpy
+            self.stats[year]['Defense'] = defense_numpy
+            
+            schedule = ds.get_schedule(year)
+            for index, row in schedule.iterrows():
+                home = self.tm.get_team_id(row['Home'])
+                away = self.tm.get_team_id(row['Away'])
+                winner = 1 if self.tm.get_team_id(row['Winner/tie']) == home else 0
+                
+                data = [offense_numpy[home].tolist(), 
+                        defense_numpy[home].tolist(),
+                        offense_numpy[away].tolist(),
+                        defense_numpy[away].tolist()]           
+                
+                self.train_data.append(data)
+                self.train_results.append(winner)
+          
+    
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.Flatten(input_shape=(4, len(self.train_data[0][0]))),
+            tf.keras.layers.Dense(10, activation='relu'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(2)
+            ])
+        
+        self.model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
+    
+        self.model.fit(self.train_data, self.train_results, epochs=50, verbose=False)      
+        self.probability_model = tf.keras.Sequential([self.model, tf.keras.layers.Softmax()])
+        
+    def predict_winner(self, home, away, year):
+        if not year in self.stats.keys():
+            print(f'{year} was not included in data')
+            return
+        
+        data = [self.stats[year]['Offense'][home].tolist(), 
+                self.stats[year]['Defense'][home].tolist(), 
+                self.stats[year]['Offense'][away].tolist(), 
+                self.stats[year]['Defense'][away].tolist()]     
+        data_exp = (np.expand_dims(data,0))
+        
+        result = self.probability_model.predict(data_exp)
+     
+        return home if np.argmax(result) == 1 else away, np.max(result)   
+            
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
