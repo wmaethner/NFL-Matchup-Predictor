5de9cc135ddd43c5d08ad2743a9d98ffb22119e7
Will Maethner <wmaethner@gmail.com>
2022-02-22 19:09:50 -0500

    Handle stats by week

--- src/nfl/Core/Console.py
+++ src/nfl/Core/Console.py
@@ -8,6 +8,8 @@ Created on Mon Jan 31 21:57:47 2022
 
 import os
 import typing
+import pickle
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -61,18 +63,6 @@ def make_prediction(year, home, away):
     # print(stats.predict_winner(home, away))
     # print(off_def.predict_winner(home, away))
 
-def analyze_predicters(train_years, test_years):
-    off = Offense_Correlation(train_years, False)
-    both = Offense_Correlation(train_years, True)
-    tfb = TensorFlowBasic(train_years, False)
-    tfb_both = TensorFlowBasic(train_years, True)
-   
-    analyze_predicter(off, test_years)
-    analyze_predicter(both, test_years)
-    analyze_predicter(tfb, test_years)
-    analyze_predicter(tfb_both, test_years)
-
-       
 def save_stats_to_csvs(years):
     tm = Team_Manager()
 
@@ -91,13 +81,192 @@ def save_stats_to_csvs(years):
         defense.to_csv(f'CSVs/{year}_defense.csv')  
 
 
+def analyze_predicters(train_years, test_years):
+    data = []
+    
+    s = start_timer('Build OffCorr')
+    off = Offense_Correlation(train_years, False)
+    data.append(stop_timer(s))
+    
+    s = start_timer('Build OffCorrDef')
+    both = Offense_Correlation(train_years, True)
+    data.append(stop_timer(s))
+    
+    s = start_timer('Build Tensor')
+    tfb = TensorFlowBasic(train_years, False)
+    data.append(stop_timer(s))
+    
+    s = start_timer('Build TensorDef')
+    tfb_both = TensorFlowBasic(train_years, True)
+    data.append(stop_timer(s))
+    
+    s = start_timer('Predict OffCorr')
+    analyze_predicter(off, test_years)
+    data.append(stop_timer(s))
+    
+    s = start_timer('Predict OffCorrDef')
+    analyze_predicter(both, test_years)
+    data.append(stop_timer(s))
+    
+    s = start_timer('Predict Tensor')
+    analyze_predicter(tfb, test_years)
+    data.append(stop_timer(s))
+    
+    s = start_timer('Predict TensorDef')
+    analyze_predicter(tfb_both, test_years)
+    data.append(stop_timer(s))
+    
+    for x in data:
+        print(f'{x["label"]}: {x["elapsed"]}')
+      
+
 def main():
-    pd.set_option('display.max_columns', None)
+    # pd.set_option('display.max_columns', None)
+    
+    off = Offense_Correlation([2021], False)
+   
     
     ds = Data_Scraper()
     tm = Team_Manager()
     
+    # data = tm.get_teams_season_stats(0, 2021)
+    # print(data)
+    data = tm.get_teams_stats_by_week(0, 2021, all_weeks=True, sum_results=False, only_data_columns=True)
+    # data = tm.get_teams_stats_by_week(0, 2021, all_weeks=True, sum_results=True, only_data_columns=False)
+    # print(data.columns)
+    # print(data[[('Offense'),('Defense'),('Meta','Home'),('Meta','Win'),('Meta','Loss'),('Meta','Tie')]])
+    stats_normal = (data-data.min())/(data.max()-data.min())
+    stats_normal.fillna(0, inplace=True)
+    corr = stats_normal.corr() 
+    print(stats_normal)
+    print(corr)
+    print(corr[('Meta','Win')])
+    
+    # data = tm.get_teams_stats_by_week(0, 2021, 15, all_weeks=True, sum_results=True)
+    # print(data)
+    # print(ds.season_stats_by_week)
+    # data = ds.get_teams_weekly_stats('crd', 2021)
+    # print(data)
+    # print(data)
+    # print(data.columns)
+    # idx = data.columns
+    
+    # # print(idx)
+    # idx = pd.MultiIndex.from_tuples([('Meta' if 'Unnamed' in x[0] else x[0],x[1]) for x in idx])
+    # # print(idx)
+    # data.columns = idx
+    # print(data)
+    # data.columns.set_levels(['b1','c1','f1'],level=1,inplace=True)
+    # print(data)
+    
+    # print(ds.season_stats)
+    # data = ds.get_teams_stats('crd', 2021)
+    # print(data)
+    # print(data.loc[['Offense']])
+    # return
+    # print(ds.stats)
+    # data = ds.get_teams_season_stats('crd', 2021)
+    # print(data)
    
+    # data = ds.get_teams_stats_1('crd', 2018)
+    # print(data)
+    
+    # data = ds.load_teams_season_stats('crd', 2021)
+    # print(data)
+    
+    
+    # df1 = pd.DataFrame(np.arange(40).reshape(10, 4),
+    #               columns=[[1,1,2,2],['A', 'B', 'C', 'D']])
+    
+    # print(df1)
+    
+    # df1.columns = df1.columns.set_levels([[1,2],[1,2,3,4]])
+    # print(df1)
+    
+    
+    # idx = df1.index.array
+    # to_remove = [x for x in idx if x > 5]
+    # df1.drop(to_remove, inplace=True)
+    
+    # print(df1)
+    
+    # df2 = pd.DataFrame(np.arange(12).reshape(3, 4),
+    #               columns=['A', 'B', 'C', 'D'])
+    # df2.iloc[1] = df2.iloc[1].apply(lambda x: x + 1)
+    
+    # df_first = pd.concat({'Foo': df1, 'Bar': df2})
+
+    # df3 = pd.DataFrame(np.arange(12).reshape(3, 4),
+    #               columns=['A', 'B', 'C', 'D'])
+    # df4 = pd.DataFrame(np.arange(12).reshape(3, 4),
+    #               columns=['A', 'B', 'C', 'D'])
+    # df4.iloc[1] = df2.iloc[1].apply(lambda x: x + 5)
+    
+    # df_second = pd.concat({'Foo': df3, 'Bar': df4})
+    
+    # df_all = pd.concat({'first': df_first, 'second': df_second})
+    # # print(df_first)
+    # # print(df_second)
+    # print(df_all)
+    # print(df_all.loc[('second', 'Bar')])
+    
+  
+    
+    # test = {'A': np.arange(100).reshape(10,10),
+    #         'B': np.arange(100).reshape(10,10),
+    #         'C': np.arange(100).reshape(10,10),
+    #         'D': np.arange(100).reshape(10,10) }
+    
+    # s = start_timer('save dict')
+    # f = open(f'testdict.pkl', 'wb')
+    # pickle.dump(test, f)
+    # f.close()
+    # stop_timer(s, True)
+    
+
+    # test_df = pd.DataFrame(test, columns=list(range(10)))
+
+
+    # s = start_timer('save df')    
+    # test_df.to_pickle('testdf.pkl')
+    # stop_timer(s, True)    
+
+    # s = start_timer('load dict')
+    # with open(f'testdict.pkl', 'rb') as p:
+    #     test = pickle.load(p)
+    # stop_timer(s, True)    
+
+    # s = start_timer('load df')
+    # test_df = pd.read_pickle("testdf.pkl")    
+    # stop_timer(s, True)  
+    
+
+    
+    
+    
+    
+    
+    
+    # ds.get_teams_weekly_stats('crd', 2021)
+    
+    # data = []
+    # train_years = [2017,2018,2019,2020]
+    # test_years = [2021]
+    # analyze_predicters(train_years, test_years)
+    
+    
+    # s = start_timer('Build Tensor')
+    # tfb = TensorFlowBasic(train_years, False)
+    # stop_timer(s)
+    
+    # s = start_timer('Build TensorDef')
+    # tfb_both = TensorFlowBasic(train_years, True)
+    # data.append(stop_timer(s))
+    
+    return
+
+
+
     # train_years = [2017]
     # test_years = [2017]
     # for i in range(5):
@@ -107,10 +276,6 @@ def main():
     #     analyze_predicter(tfbs, test_years)
     #     print('========================')
     # return
-    
-    analyze_predicters([2017,2018,2019,2020,2021], [2020,2021])
-    
-    return
     
     # print(tfb.train_data)
     # print(len(tfb.train_results))
--- src/nfl/Data/DataScraper.py
+++ src/nfl/Data/DataScraper.py
@@ -8,8 +8,9 @@ Created on Mon Jan 31 19:43:08 2022
 
 import pandas as pd
 
-from nfl.Core.utilities import (PFR_BASE_URL, parse_page, get_table_by_id, parse_table,
-                       save_obj, load_obj, file_exists)
+from nfl.Core.utilities import (PFR_BASE_URL, parse_page, get_table_by_id, 
+                                parse_table,save_obj, load_obj, file_exists,
+                                save_df, load_df)
 
 base_url = "https://www.pro-football-reference.com"
 teams = []
@@ -19,6 +20,10 @@ stats = {}
 class Data_Scraper:  
     def __init__(self):
         self.stats = {}
+        self.season_stats = load_df('season_stats') if file_exists('season_stats.pkl') else pd.DataFrame()
+        self.season_stats_by_week = load_df('season_stats_by_week') if file_exists('season_stats_by_week.pkl') else pd.DataFrame()
+        self.schedule = load_df('schedule') if file_exists('schedule.pkl') else pd.DataFrame()
+
     
     def data_columns(self):
         return ['Wins','Losses','Ties','PF','Yds','Ply','Y/P','TO','FL','1stD',
@@ -75,33 +80,131 @@ class Data_Scraper:
         self.stats[team_key][year]['Defense'] = [float(x) for x in defense]
         
         save_obj(f'{team_key}.pkl', self.stats[team_key])
+    
+    def get_teams_season_stats(self, team_key, year):       
+        if team_key in self.season_stats.index.unique(0).array:
+            if not year in self.season_stats.loc[team_key].index.unique(0).array:
+                df_year = self.load_teams_season_stats(team_key, year)
+                df_indexed = pd.concat({year: df_year})
+                self.season_stats = pd.concat([self.season_stats, pd.concat({team_key: df_indexed})])
+                save_df('season_stats', self.season_stats)
+        else:
+            df_year = self.load_teams_season_stats(team_key, year)
+            df_indexed = pd.concat({year: df_year})
+            self.season_stats = pd.concat([self.season_stats, pd.concat({team_key: df_indexed})])
+            save_df('season_stats', self.season_stats)
+        
+        self.season_stats = self.season_stats.sort_index()
+        return self.season_stats.loc[(team_key, year)]
+    
+    def get_teams_season_stats_by_week(self, team_key, year, week):
+        if team_key in self.season_stats_by_week.index.unique(0).array:
+            if not year in self.season_stats_by_week.loc[team_key].index.unique(0).array:
+                df_year = self.get_teams_weekly_stats(team_key, year)
+                df_indexed = pd.concat({year: df_year})
+                self.season_stats_by_week = pd.concat([self.season_stats_by_week, pd.concat({team_key: df_indexed})])
+                save_df('season_stats_by_week', self.season_stats_by_week)
+        else:
+            df_year = self.get_teams_weekly_stats(team_key, year)
+            df_indexed = pd.concat({year: df_year})
+            self.season_stats_by_week = pd.concat([self.season_stats_by_week, pd.concat({team_key: df_indexed})])
+            save_df('season_stats_by_week', self.season_stats_by_week)
+        
+        self.season_stats_by_week = self.season_stats_by_week.sort_index()
+        return self.season_stats_by_week.loc[(team_key, year)]  
 
-    def get_teams_stats(self, team_key, year, fmt = 0):
-        if not team_key in self.stats.keys():
-            self.load_stats(team_key, year)
-            
-        if not year in self.stats[team_key].keys():
-            self.load_stats(team_key, year)
-        
-        if fmt == 0:
-            return self.stats[team_key][year]
-        elif fmt == 1:
-            df = pd.DataFrame(self.stats[team_key][year]).T
-            df.columns = self.data_columns()
-            return df
-        
-    def get_schedule(self, year):
-        url = PFR_BASE_URL + f"/years/{year}/games.htm"
+    def get_teams_weekly_stats(self, team_key, year):
+        url = PFR_BASE_URL + f"/teams/{team_key}/{year}.htm"
         soup = parse_page(url)
         table = get_table_by_id(soup, 'games')
-
         df = pd.read_html(str(table))[0]
-        df_filtered = df[df['Week'] != 'Week']
-        df_filtered = df_filtered[df_filtered['Date'] != 'Playoffs']
 
-        df_filtered.loc[:, 'Home'] = df_filtered.apply(lambda row: row['Loser/tie'] if row[5] == '@' else row['Winner/tie'], axis=1)
-        df_filtered.loc[:, 'Away'] = df_filtered.apply(lambda row: row['Winner/tie'] if row[5] == '@' else row['Loser/tie']   , axis=1)
+        unnamed_vals = ['Time','Boxscore','Result','Home']        
+        df.columns = pd.MultiIndex.from_tuples(
+            [('Meta' if 'Unnamed' in x[0] else x[0],
+              unnamed_vals.pop(0) if 'Unnamed' in x[1] else x[1]) 
+             for x in df.columns])
 
-        df_filtered = df_filtered.drop(columns=[df_filtered.columns[5], df_filtered.columns[7]])
+        # Drop Expected points and box score columns
+        df.drop(columns=('Expected Points'), level=0, inplace=True)
+        df.drop(columns=('Meta','Boxscore'), inplace=True)
+        
+        # Drop bye week row
+        df.drop(df[df[('Meta','Opp')] == 'Bye Week'].index, inplace = True)
+        
+        # Drop playoffs
+        playoff_idx = min(df[df[('Meta','Date')] == 'Playoffs'].index.array)
+        df.drop([x for x in df.index.array if x >= playoff_idx], inplace=True)
+        
+        # Set Home and Won columns
+        df.loc[:, ('Meta','Home')] = df.apply(lambda row: 0 if row[('Meta','Home')] == '@' else 1, axis=1)
+        df.loc[:, ('Meta', 'Win')] = df.apply(lambda row: 1 if row[('Meta','Result')] == 'W' else 0, axis=1)
+        df.loc[:, ('Meta', 'Loss')] = df.apply(lambda row: 1 if row[('Meta','Result')] == 'L' else 0, axis=1)
+        df.loc[:, ('Meta', 'Tie')] = df.apply(lambda row: 1 if row[('Meta','Result')] == 'T' else 0, axis=1)
+    
+        df.fillna(0, inplace=True)
+        return df
+        
+        
+        
+    def get_schedule(self, year):
+        if not year in self.schedule.index.unique(0).array:        
+            url = PFR_BASE_URL + f"/years/{year}/games.htm"
+            soup = parse_page(url)
+            table = get_table_by_id(soup, 'games')
+    
+            df = pd.read_html(str(table))[0]
+            df_filtered = df[df['Week'] != 'Week']
+            df_filtered = df_filtered[df_filtered['Date'] != 'Playoffs']
+    
+            df_filtered.loc[:, 'Home'] = df_filtered.apply(lambda row: row['Loser/tie'] if row[5] == '@' else row['Winner/tie'], axis=1)
+            df_filtered.loc[:, 'Away'] = df_filtered.apply(lambda row: row['Winner/tie'] if row[5] == '@' else row['Loser/tie']   , axis=1)
+    
+            df_filtered = df_filtered.drop(columns=[df_filtered.columns[5], df_filtered.columns[7]])
+            self.schedule = pd.concat([self.schedule, pd.concat({year: df_filtered})]) 
+            
+            save_df('schedule', self.schedule)
 
-        return df_filtered
+        return self.schedule.loc[year]
+    
+    
+    def load_teams_season_stats(self, team_key, year):
+        url = PFR_BASE_URL + f"/teams/{team_key}/{year}.htm"
+        soup = parse_page(url)
+        
+        # Could get this info simpler from the main season page, but the parsing
+        # takes forever so its quicker to just scrape through the team data
+        meta_div = soup.find_all('div', {'data-template': 'Partials/Teams/Summary'})[0]
+        record_p = meta_div.find_all('p')[0]
+        end_tag = '</strong>'
+        end_tag_index = str(record_p).find(end_tag)
+        comma_index = str(record_p).find(',',end_tag_index)
+        record = str(record_p)[end_tag_index+len(end_tag)+1:comma_index]
+        record_parts = record.split('-')
+        record_vals = [int(x) for x in record_parts]
+        
+        table = get_table_by_id(soup, 'team_stats')
+        parsed_rows = parse_table(table)
+        
+        offense = [record_vals[0],record_vals[1],record_vals[2]]
+        defense = [record_vals[0],record_vals[1],record_vals[2]]
+        
+        offense.extend(parsed_rows[2])
+        defense.extend(parsed_rows[3])
+        
+        start_index = self.data_columns().index('Start')
+        time_index = self.data_columns().index('Time')
+        
+        offense[start_index] = offense[start_index].split(' ')[1]
+        defense[start_index] = defense[start_index].split(' ')[1]
+        
+        time = offense[time_index].split(':')
+        offense[time_index]  = int(time[0]) + (int(time[1]) / 60)
+
+        time = defense[time_index].split(':')
+        defense[time_index]  = int(time[0]) + (int(time[1]) / 60)
+        
+        df = pd.DataFrame({'Offense': [float(x) for x in offense], 
+                           'Defense': [float(x) for x in defense]}).T
+        df.columns = self.data_columns()
+        return df
